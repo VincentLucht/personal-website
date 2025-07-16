5a5b5f06994e21aa89e2f0bf4e2ce726eb0becb9
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-07-17 00:35:57 +0200

    Reduce padding and margin on mobile

--- src/code/Skills/SkillContainer.tsx
+++ src/code/Skills/SkillContainer.tsx
@@ -1,3 +1,5 @@
+import useGetScreenSize from '@/code/hooks/useGetScreenSize';
+
 interface SkillContainerProps {
   name: string;
   children: React.ReactNode;
@@ -11,13 +13,19 @@ export default function SkillContainer({
   className,
   childrenClassName,
 }: SkillContainerProps) {
+  const { width } = useGetScreenSize();
+
   return (
     <div
-      className={`skill-container flex flex-col gap-6 rounded-3xl bg-neutral-800 p-8 ${className}`}
+      className={`skill-container flex flex-col gap-6 rounded-3xl bg-neutral-800
+        ${width <= 500 ? 'p-6' : 'p-8'} ${className}`}
     >
       {name && <h3 className="text-3xl">{name}</h3>}
 
-      <div className={`flex flex-wrap items-center gap-3 ${childrenClassName}`}>
+      <div
+        className={`flex flex-wrap items-center gap-3 ${width <= 500 ? 'justify-center' : ''}
+          ${childrenClassName}`}
+      >
         {children}
       </div>
     </div>
