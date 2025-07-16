2c9fbe7ff653e4794a18260f8d42dd8c0a653f51
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-07-17 00:35:21 +0200

    Center text on mobile

--- src/code/Intro.tsx
+++ src/code/Intro.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useRef } from 'react';
 import { useTranslation } from 'react-i18next';
+import useGetScreenSize from '@/code/hooks/useGetScreenSize';
 import Typed from 'typed.js';
 import '@/css/App.css';
 
 export default function Intro() {
   const typedRef = useRef(null);
   const { t } = useTranslation('intro');
+  const { width } = useGetScreenSize();
 
   useEffect(() => {
     const typed = new Typed(typedRef.current, {
@@ -22,13 +24,13 @@ export default function Intro() {
   }, []);
 
   return (
-    <div className="mb-[120px] mt-[100px] df">
+    <div className={`mb-[120px] mt-[100px] df ${width <= 500 ? 'text-center' : ''}`}>
       <h1 className="flex flex-col text-6xl">
         <div>
           {t('greeting')} <span className="wave">👋</span> {t('name')}
         </div>
 
-        <div className="flex">
+        <div className={`flex ${width <= 500 ? 'justify-center' : ''}`}>
           {t('article')}
           <div ref={typedRef} className="ml-3 text-blue-500" id="typed"></div>
         </div>
